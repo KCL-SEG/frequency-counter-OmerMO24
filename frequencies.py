7929a9ad29442708f179160a4c406abeeb27995b
Omer Osman <omer.m.o.hussain@gmail.com>
2023-12-04 15:31:11 +0000

    Solve frequencies

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    mappedList = map(str, items)
+
+    items = list(mappedList)
+
+    for i in items:
+        x = items.count(i)
+        frequencies.update({i : x})
+        
     return frequencies
